5e0973a1c40ca8dd627104242afdaecd4e69d2fc
DELL <DELL@DESKTOP-EBJGPVB>
2022-02-20 23:55:18 +0300

    commmit

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
 
 const AllEventsPage = () => {
+  const events = getAllEvents();
   return (
     <div>
-        <h1>All Events</h1>
+        <EventList items={events} />
     </div>
   )
 }
